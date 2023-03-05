e2473a2d4c674e846ade229cbd23cf4118a6dcb5
Zain Ahmed <zainahmed01997@gmail.com>
2023-03-05 21:09:25 +0530

    first commit

---
+++ app/src/main/java/com/bawp/todoister/util/TaskRoomDatabase.java
@@ -0,0 +1,60 @@
+package com.bawp.todoister.util;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.bawp.todoister.data.TaskDao;
+import com.bawp.todoister.model.Task;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Task.class} , version = 1 , exportSchema = false)
+public abstract class TaskRoomDatabase extends RoomDatabase {
+
+    public static final int NUMBER_OF_THREADS = 4; // WE CAN ADD ANY NUMBER
+
+    public static final String DATABASE_NAME = "todoister_database";
+
+    private static volatile TaskRoomDatabase INSTANCE;
+
+    public static final ExecutorService databaseWriterExecutor
+            = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    public static final RoomDatabase.Callback sRoomDatabaseCallback
+            = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db){
+            super.onCreate(db);
+            databaseWriterExecutor.execute(() -> {
+                //invoke Dao  , and write
+                TaskDao taskDao = INSTANCE.taskDao();
+                taskDao.deleteAll(); // clean slate !
+
+                // write to our table
+
+            });
+        }
+    };
+
+    public static TaskRoomDatabase getDatabase(final Context context){
+        if (INSTANCE == null) {
+            synchronized (TaskRoomDatabase.class){
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    TaskRoomDatabase.class,DATABASE_NAME)
+                                    .addCallback(sRoomDatabaseCallback)
+                                    .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public abstract TaskDao taskDao();
+}
