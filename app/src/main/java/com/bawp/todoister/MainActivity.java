035e1872454f8373d026546709676e6761f3eb84
Zain Ahmed <zainahmed01997@gmail.com>
2023-03-06 22:58:36 +0530

    time stamp : 1:09:00

--- app/src/main/java/com/bawp/todoister/MainActivity.java
+++ app/src/main/java/com/bawp/todoister/MainActivity.java
@@ -2,20 +2,28 @@ package com.bawp.todoister;
 
 import android.os.Bundle;
 
+import com.bawp.todoister.model.Priority;
+import com.bawp.todoister.model.Task;
 import com.bawp.todoister.model.TaskViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.Calendar;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "ITEM";
     private TaskViewModel taskViewModel;
 
     @Override
@@ -27,14 +35,17 @@ public class MainActivity extends AppCompatActivity {
         taskViewModel = new ViewModelProvider.AndroidViewModelFactory(MainActivity.this.getApplication())
                         .create(TaskViewModel.class);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-                
+        taskViewModel.getAllTasks().observe(this, tasks ->   {
+            for (Task task : tasks) {
+                Log.d(TAG, "onCreate: " + task.getTask());
             }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view ->  {
+            Task task = new Task("Todo", Priority.HIGH, Calendar.getInstance().getTime(),
+                                Calendar.getInstance().getTime(),false);
+            TaskViewModel.insert(task);
         });
     }
 
